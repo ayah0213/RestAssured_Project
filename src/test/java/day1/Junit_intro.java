b1f6a3671ed5c5842fcf56cf83f97bf94b925ed0
Ayah N <ayah.noor0213@gmail.com>
2021-03-30 15:05:50 -0400

    2

---
+++ src/test/java/day1/Junit_intro.java
@@ -0,0 +1,15 @@
+package day1;
+
+import org.junit.jupiter.api.Test;
+
+public class Junit_intro {
+
+
+    @Test
+    public void test(){
+        System.out.println("LEarning junit 5");
+    }
+
+
+
+}
